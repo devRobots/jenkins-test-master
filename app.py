ed8079c4c656a4f470d2ed518bfacc184360d405
rosastoy <yesid.rosas@famytec.com>
2025-03-27 16:42:31 +0100

    better import

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-import importlib
+import os
+import sys
 
-logfunctions = importlib.import_module("b2b-modules.logfunctions")
-logfunctions.log_message("100", "Funciona!")
+ruta_modulos_b2b = os.path.join(os.path.dirname(__file__), "b2b-modules")
+
+if os.path.exists(ruta_modulos_b2b):
+    ruta_absoluta = os.path.abspath(ruta_modulos_b2b)
+    if ruta_absoluta not in sys.path:
+        sys.path.append(ruta_absoluta)
+
+    from logfunctions import log_message
+    log_message("100", "Funciona!!!")
+else:
+    print(f"Error: La ruta '{ruta_modulos_b2b}' no existe.")
